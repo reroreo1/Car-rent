257592ed59b6cdbff1f02b3808c3d4dc2b691664
mohamed Elkarmi <Elkarmimed@gmail.com>
2023-09-23 00:37:50 +0100

    first commit

---
+++ components/CarCard.vue
@@ -0,0 +1,90 @@
+<template>
+    <div class="p-[24px] drop-shadow flex flex-col rounded-xl bg-white min-w-[305px]  my-2 max-w-[305px]">
+        <div class="flex items-center justify-between">
+            Koenigsegg
+            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="18" viewBox="0 0 20 18" fill="none">
+                <path
+                    d="M14.44 0.100098C12.63 0.100098 11.01 0.980098 10 2.3301C8.99 0.980098 7.37 0.100098 5.56 0.100098C2.49 0.100098 0 2.6001 0 5.6901C0 6.8801 0.19 7.9801 0.52 9.0001C2.1 14.0001 6.97 16.9901 9.38 17.8101C9.72 17.9301 10.28 17.9301 10.62 17.8101C13.03 16.9901 17.9 14.0001 19.48 9.0001C19.81 7.9801 20 6.8801 20 5.6901C20 2.6001 17.51 0.100098 14.44 0.100098Z"
+                    fill="#ED3F3F" />
+            </svg>
+        </div>
+        <div class="flex items-center text-sm justify-between text-[#90A3BF]">
+            Sport
+        </div>
+        <img src="../assets/car1.png" alt="" class="w-auto mx-auto my-20 h-[70px] ">
+        <div class="flex items-center text-sm justify-between text-[#90A3BF]">
+            <span class="flex items-center gap-2">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                    <path
+                        d="M22.34 9.33L20.34 8.33C19.97 8.15 19.51 8.29 19.33 8.66C19.14 9.04 19.29 9.49 19.66 9.67L21.25 10.46V15.25L17.5 15.26V5C17.5 3 16.16 2 14.5 2H6.5C4.84 2 3.5 3 3.5 5V21.25H2C1.59 21.25 1.25 21.59 1.25 22C1.25 22.41 1.59 22.75 2 22.75H19C19.41 22.75 19.75 22.41 19.75 22C19.75 21.59 19.41 21.25 19 21.25H17.5V16.76L22 16.75C22.42 16.75 22.75 16.41 22.75 16V10C22.75 9.72 22.59 9.46 22.34 9.33ZM6 6.89C6 5.5 6.85 5 7.89 5H13.12C14.15 5 15 5.5 15 6.89V8.12C15 9.5 14.15 10 13.11 10H7.89C6.85 10 6 9.5 6 8.11V6.89ZM6.5 12.25H9.5C9.91 12.25 10.25 12.59 10.25 13C10.25 13.41 9.91 13.75 9.5 13.75H6.5C6.09 13.75 5.75 13.41 5.75 13C5.75 12.59 6.09 12.25 6.5 12.25Z"
+                        fill="#90A3BF" />
+                </svg>
+                70L
+            </span>
+            <span class="flex items-center gap-2">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                    <path d="M12 2C6.48 2 2 6.48 2 12C2 17.52 6.48 22 12 22C17.52 22 22 17.52 22 12C22 6.48 17.53 2 12 2Z"
+                        fill="#90A3BF" />
+                    <rect x="4" y="4" width="16" height="16" rx="8" fill="white" />
+                    <path
+                        d="M12 6C8.688 6 6 8.688 6 12C6 15.312 8.688 18 12 18C15.312 18 18 15.312 18 12C18 8.688 15.318 6 12 6Z"
+                        fill="#90A3BF" />
+                    <rect x="8" y="8" width="8" height="8" rx="4" fill="white" />
+                    <rect x="11" y="17" width="2" height="4" fill="#90A3BF" />
+                    <rect x="17" y="11" width="4" height="2" fill="#90A3BF" />
+                    <rect x="3" y="11" width="4" height="2" fill="#90A3BF" />
+                </svg>
+                Manual
+            </span>
+            <span class="flex items-center gap-2">
+
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                    <path
+                        d="M9 2C6.38 2 4.25 4.13 4.25 6.75C4.25 9.32 6.26 11.4 8.88 11.49C8.96 11.48 9.04 11.48 9.1 11.49C9.12 11.49 9.13 11.49 9.15 11.49C9.16 11.49 9.16 11.49 9.17 11.49C11.73 11.4 13.74 9.32 13.75 6.75C13.75 4.13 11.62 2 9 2Z"
+                        fill="#90A3BF" />
+                    <path
+                        d="M14.08 14.1499C11.29 12.2899 6.73996 12.2899 3.92996 14.1499C2.65996 14.9999 1.95996 16.1499 1.95996 17.3799C1.95996 18.6099 2.65996 19.7499 3.91996 20.5899C5.31996 21.5299 7.15996 21.9999 8.99996 21.9999C10.84 21.9999 12.68 21.5299 14.08 20.5899C15.34 19.7399 16.04 18.5999 16.04 17.3599C16.03 16.1299 15.34 14.9899 14.08 14.1499Z"
+                        fill="#90A3BF" />
+                    <path
+                        d="M19.99 7.3401C20.15 9.2801 18.77 10.9801 16.86 11.2101C16.85 11.2101 16.85 11.2101 16.84 11.2101H16.81C16.75 11.2101 16.69 11.2101 16.64 11.2301C15.67 11.2801 14.78 10.9701 14.11 10.4001C15.14 9.4801 15.73 8.1001 15.61 6.6001C15.54 5.7901 15.26 5.0501 14.84 4.4201C15.22 4.2301 15.66 4.1101 16.11 4.0701C18.07 3.9001 19.82 5.3601 19.99 7.3401Z"
+                        fill="#90A3BF" />
+                    <path
+                        d="M21.99 16.5899C21.91 17.5599 21.29 18.3999 20.25 18.9699C19.25 19.5199 17.99 19.7799 16.74 19.7499C17.46 19.0999 17.88 18.2899 17.96 17.4299C18.06 16.1899 17.47 14.9999 16.29 14.0499C15.62 13.5199 14.84 13.0999 13.99 12.7899C16.2 12.1499 18.98 12.5799 20.69 13.9599C21.61 14.6999 22.08 15.6299 21.99 16.5899Z"
+                        fill="#90A3BF" />
+                </svg>
+                6 People
+            </span>
+        </div>
+        <div class="flex items-center  justify-between text-[#90A3BF] mt-3">
+            <div>
+                <div class="text-[#1A202C] text-xl font-semibold">
+                    $17.00/<span class="text-sm font-[500] text-[#90A3BF]">
+                        day
+                    </span>
+                </div>
+                <span class="line-through text-[#90A3BF]">
+                    $80.00
+                </span>
+            </div>
+            <button class="button">
+                    Rental Car
+            </button>
+
+        </div>
+    </div>
+</template>
+
+<style>
+.button {
+    @apply border border-[#3563E9] bg-[#3563E9] w-auto rounded-lg text-white px-4 py-2 transition;
+
+}
+
+.button:hover {
+    @apply border border-[#3563E9] bg-[white] text-[#3563E9];
+}
+
+.button:active {
+    @apply scale-[0.95];
+}
+</style>
---
+++ components/CarRentalcard.vue
@@ -0,0 +1,218 @@
+<template class="">
+    <div class="bg-[#F6F7F9]">
+
+        <div class="  flex items-center max-lg:flex-col max-lg:gap-3 gap-[32px] container mx-auto">
+            <div class="bg-[#54A6FF] flex-1 cartone-1 rounded-xl my-5 p-[24px] text-white flex flex-col items-start ">
+                <span class=" text-3xl mb-2 w-[300px] font-semibold">The Best Platform</span>
+
+                <span class="text-3xl  w-[300px] font-semibold">for Car Rental</span>
+                <span class="text-lg  w-[300px] font-[400] mt-[16px]">Ease of doing a car rental safely and reliably. Of
+                    course
+                    at a low price.</span>
+                <button class="button w-auto mt-3">
+                    Rental Car
+                </button>
+                <div class="w-full flex items-center mt-3 justify-center">
+                    <img src="../assets/car1.png" alt="" class="w-auto h-[100px]">
+                </div>
+            </div>
+            <div class="bg-[#3563E9] flex-1 cartone-2 rounded-xl  my-5 p-[24px] text-white flex flex-col items-start ">
+                <span class=" text-3xl mb-2 w-[300px] font-semibold">Easy way to rent a</span>
+
+                <span class="text-3xl  w-[300px] font-semibold">car at a low price</span>
+                <span class="text-lg  w-[300px] font-[400] mt-[16px]">Providing cheap car rental services and safe and
+                    comfortable facilities.</span>
+                <button class="button2 bg-[#54A6FF] w-auto mt-3">
+                    Rental Car
+                </button>
+                <div class="w-full flex items-center  mt-3 justify-center">
+                    <img src="../assets/car2.png" alt="" class="w-auto h-[100px]">
+                </div>
+            </div>
+        </div>
+        <div class="w-full my-3 container mx-auto cursor-pointer flex item-center justify-between text-[#90A3BF] text-sm">
+            Popular Car
+            <a class="hover:underline text-[#54A6FF] transition">
+                View All
+            </a>
+        </div>
+        <div class="container mx-auto my-3">
+            <carousel class=" w-full py-3 " v-bind="settings" :breakpoints="breakpoints">
+                <slide v-for="item in items" :key="item.id">
+                    <CarCard />
+                </slide>
+            </carousel>
+        </div>
+        <div class="w-full my-3 container mx-auto cursor-pointer flex item-center justify-between text-[#90A3BF] text-sm">
+            Recomendation Car
+
+        </div>
+        <div class="container mx-auto flex flex-wrap items-center justify-center ">
+
+            <CarCard />
+            <CarCard />
+            <CarCard />
+            <CarCard />
+            <CarCard />
+            <CarCard />
+            <CarCard />
+            <CarCard />
+            <CarCard />
+            <CarCard />
+            <CarCard />
+            <CarCard />
+        </div>
+        <div class="container mx-auto flex items-center ">
+
+            <button class="button w-auto mx-auto mt-3">
+                Rental Car
+            </button>
+        </div>
+    </div>
+</template>
+<style >
+.container {
+    padding: 1rem;
+}
+
+.cartone-1 {
+
+    background-image: url("../assets/patterns.png");
+    animation: animateScale 15s infinite ease-in-out;
+}
+
+.cartone-2 {
+
+    background-image: url("../assets/patterns2.png");
+    animation: animateScale 15s infinite ease-in-out;
+}
+
+.button {
+    @apply border border-[#3563E9] bg-[#3563E9] w-auto rounded-lg text-white px-4 py-2 transition;
+
+}
+
+.button:hover {
+    @apply border border-[#3563E9] bg-[white] text-[#3563E9];
+}
+
+.button:active {
+    @apply scale-[0.95];
+}
+
+.button2 {
+    @apply border border-[#54A6FF] bg-[#54A6FF] w-auto rounded-lg text-white px-4 py-2 transition;
+
+}
+
+.button2:hover {
+    @apply border border-[#54A6FF] bg-[white] text-[#54A6FF];
+}
+
+.button2:active {
+    @apply scale-[0.95];
+}
+
+/* .cartone-2 {
+    background-image: url("../assets/BG.png");
+    background-size: 105% 105%;
+    background-position: 30% 30%;
+    animation: Move 7s ease-in-out infinite;
+} */
+
+@keyframes animateScale {
+    0% {
+        background-size: 100%;
+    }
+
+    25% {
+        background-size: 110%;
+
+    }
+
+    50% {
+        background-size: 120%;
+
+    }
+
+    75% {
+        background-size: 110%;
+    }
+
+    100% {
+        background-size: 100%;
+
+    }
+}
+
+@keyframes Move {
+    0% {
+        background-position: 0% 0%;
+    }
+
+    25% {
+        background-position: 100% 0%;
+    }
+
+    50% {
+        background-position: 120% 0%;
+
+    }
+
+    75% {
+        background-position: 100% 0%;
+    }
+
+    100% {
+        background-position: 0% 0%;
+    }
+}
+</style>
+
+<script>
+import { Carousel, Slide } from 'vue3-carousel';
+import "vue3-carousel/dist/carousel.css";
+export default {
+    components: {
+        Carousel,
+        Slide,
+    },
+    data() {
+        return {
+            items: [
+                { id: 1, image: 'path-to-image-1.jpg' },
+                { id: 2, image: 'path-to-image-2.jpg' },
+                { id: 3, image: 'path-to-image-2.jpg' },
+                { id: 5, image: 'path-to-image-2.jpg' },
+                { id: 5, image: 'path-to-image-2.jpg' },
+                { id: 5, image: 'path-to-image-2.jpg' },
+                // Add more items as needed
+            ],
+
+            // carousel settings
+            settings: {
+
+                snapAlign: 'center',
+            },
+            // breakpoints are mobile first
+            // any settings not specified will fallback to the carousel settings
+            breakpoints: {
+                640: {
+                    itemsToShow: 1.5,
+                    snapAlign: 'start',
+                },
+                768: {
+                    itemsToShow: 2,
+                },
+                1024: {
+                    itemsToShow: 3,
+                },
+                1280: {
+                    itemsToShow: 4,
+                },
+            },
+
+        };
+    },
+};
+</script>
